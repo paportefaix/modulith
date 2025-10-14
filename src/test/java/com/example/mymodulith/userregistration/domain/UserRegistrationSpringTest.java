f8be91bf4de6cc84f27c7b024ee7fff9d9b525a8
paportefaix <paportefaix@gmail.com>
2025-10-14 20:39:21 +0200

    Use stable version of Spring

--- src/test/java/com/example/mymodulith/userregistration/domain/UserRegistrationSpringTest.java
+++ src/test/java/com/example/mymodulith/userregistration/domain/UserRegistrationSpringTest.java
@@ -2,6 +2,7 @@ package com.example.mymodulith.userregistration.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class UserRegistrationSpringTest {
+
   @Autowired
   private MockMvc mockMvc;
 
