06fbc66034c4987ea1d07e316155bbc30e24aae7
paportefaix <paportefaix@gmail.com>
2025-10-15 22:06:07 +0200

    fix

--- src/main/java/com/example/mymodulith/userregistration/application/port/in/RegisterUserPort.java
+++ src/main/java/com/example/mymodulith/userregistration/application/port/in/RegisterUserPort.java
@@ -1,9 +1,7 @@
 package com.example.mymodulith.userregistration.application.port.in;
 
 import org.jmolecules.architecture.hexagonal.PrimaryPort;
-import org.springframework.modulith.NamedInterface;
 
-@NamedInterface("RegisterUserPort")
 @PrimaryPort
 public interface RegisterUserPort {
 
--- src/main/java/com/example/mymodulith/userregistration/application/port/in/VerifyUserPort.java
+++ src/main/java/com/example/mymodulith/userregistration/application/port/in/VerifyUserPort.java
@@ -1,9 +1,8 @@
 package com.example.mymodulith.userregistration.application.port.in;
 
 import org.jmolecules.architecture.hexagonal.PrimaryPort;
-import org.springframework.modulith.NamedInterface;
 
-@NamedInterface("VerifyUserPort")
+
 @PrimaryPort
 public interface VerifyUserPort {
 
